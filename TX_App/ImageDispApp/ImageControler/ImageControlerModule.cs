dd2f86f857e42174c48da8f2545ad51ce542daf1
OkadoHironori <hironori.ookado@glb.toshiba.co.jp>
2021-07-16 11:05:50 +0900

    画像を制御するUPDOWNツールを追加した

---
+++ TX_App/ImageDispApp/ImageControler/ImageControlerModule.cs
@@ -0,0 +1,21 @@
+﻿using ImageControler.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace ImageControler
+{
+    public class ImageControlerModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            IRegionManager regionMan = containerProvider.Resolve<IRegionManager>();
+            _ = regionMan.RegisterViewWithRegion(nameof(ImageControlerModule), typeof(Views.ChangeScaler));
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
